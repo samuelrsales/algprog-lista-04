ce5a28690a5da807022a8af17d0fe2f1dadf45fd
Samuel Sales <samuelsales943@gmail.com>
2026-04-05 12:25:15 -0300

    ex6 done

---
+++ Exercicios/Ex6MostradorNumeros.java
@@ -0,0 +1,13 @@
+public class Ex6MostradorNumeros {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 20; i++) {
+            System.out.println(i);
+        }
+
+        for (int i = 1; i <= 20; i++) {
+            System.out.print(i + " ");
+        }
+
+    }
+}
